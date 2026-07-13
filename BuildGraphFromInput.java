db8931321cec2d82ab5ab524d3ae5a515496be16
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-13 17:08:12 +0530

    HashMap & Graph: BuildGraphFromInput: in-progress v2

--- BuildGraphFromInput.java
+++ BuildGraphFromInput.java
@@ -4,6 +4,12 @@ public class BuildGraphFromInput {
 
     public static void main(String[] args) {
 
+        /*
+         * Input data coming from database/API/etc.
+         *
+         * user -> list of resources used by user
+         */
+        Map<String, List<String>> userResources = new HashMap<>();
 
     }
 }
