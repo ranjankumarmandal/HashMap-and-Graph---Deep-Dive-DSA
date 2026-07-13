e90f51330032201970c49e86d479a488405176e1
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-13 17:08:29 +0530

    HashMap & Graph: BuildGraphFromInput: in-progress v3

--- BuildGraphFromInput.java
+++ BuildGraphFromInput.java
@@ -11,5 +11,11 @@ public class BuildGraphFromInput {
          */
         Map<String, List<String>> userResources = new HashMap<>();
 
+        userResources.put("u1", Arrays.asList("ip1", "domain1"));
+        userResources.put("u2", Arrays.asList("ip1"));
+        userResources.put("u3", Arrays.asList("domain1"));
+        userResources.put("u4", Arrays.asList("ip2"));
+        userResources.put("u5", Arrays.asList("ip2", "domain2"));
+
     }
 }
