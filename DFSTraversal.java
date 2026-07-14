0709353492988d5d5701f026e64f0b07b54614e3
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-14 16:03:21 +0530

    HashMap & Graph: DFSTraversal: in-progress v3

--- DFSTraversal.java
+++ DFSTraversal.java
@@ -13,5 +13,8 @@ public class Session2ConnectedComponents {
         userResources.put("u2", Arrays.asList("ip1"));
         userResources.put("u3", Arrays.asList("domain1"));
 
+        userResources.put("u4", Arrays.asList("ip2"));
+        userResources.put("u5", Arrays.asList("ip2", "domain2"));
+
     }
 }
