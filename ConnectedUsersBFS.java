a0b72b5ecc432253a0e7d1fcbfc57268b29cfda6
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-08-02 17:26:03 +0530

    HashMap & Graph: step 2: ConnectedUsersBFS: in-progress v2

--- ConnectedUsersBFS.java
+++ ConnectedUsersBFS.java
@@ -9,5 +9,13 @@ public class ConnectedUsersBFS {
         userResources.put("u1", Arrays.asList("ip1", "domain1"));
         userResources.put("u2", Arrays.asList("ip1"));
         userResources.put("u3", Arrays.asList("domain1"));
+        userResources.put("u4", Arrays.asList("ip2"));
+        userResources.put("u5", Arrays.asList("ip2", "domain2"));
+
+        Map<String, List<String>> graph = buildGraph(userResources);
+
+        printGraph(graph);
+
+        System.out.println();
     }
 }
