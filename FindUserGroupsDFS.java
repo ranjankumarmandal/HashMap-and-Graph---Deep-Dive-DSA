e08ab93aac0a80f61dbbac5a6a162b5204834522
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-08-05 07:04:45 +0530

    HashMap & Graph: step 3: FindUserGroupsDFS: in-progress

--- FindUserGroupsDFS.java
+++ FindUserGroupsDFS.java
@@ -2,5 +2,31 @@ import java.util.*;
 
 public class FindUserGroupsDFS {
 
+    public static void main(String[] args) {
 
+        Map<String, List<String>> userResources = new HashMap<>();
+
+        userResources.put("u1", Arrays.asList("ip1", "domain1"));
+        userResources.put("u2", Arrays.asList("ip1"));
+        userResources.put("u3", Arrays.asList("domain1"));
+        userResources.put("u4", Arrays.asList("ip2"));
+        userResources.put("u5", Arrays.asList("ip2", "domain2"));
+
+        Map<String, List<String>> graph = buildGraph(userResources);
+
+        printGraph(graph);
+
+        System.out.println("\nConnected User Groups:");
+
+        List<Set<String>> groups = findGroups(graph);
+
+        int groupNumber = 1;
+
+        for (Set<String> group : groups) {
+            System.out.println(
+                    "Group " + groupNumber + " : " + group
+            );
+            groupNumber++;
+        }
+    }
 }
